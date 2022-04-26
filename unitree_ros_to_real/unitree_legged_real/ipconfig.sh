1ea5a0ce25f6c4ebf49e83c67b5aa0df31fd7673
kimsooyoung <tge1375@naver.com>
2022-04-27 01:32:05 +0900

    Update : ipconfig for local setup

--- unitree_ros_to_real/unitree_legged_real/ipconfig.sh
+++ unitree_ros_to_real/unitree_legged_real/ipconfig.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo ifconfig lo multicast
-sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
+# sudo ifconfig lo multicast
+# sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
 
-sudo ifconfig enp2s0 down
-sudo ifconfig enp2s0 up 192.168.123.162 netmask 255.255.255.0
+# sudo ifconfig enp2s0 down
+# sudo ifconfig enp2s0 up 192.168.123.162 netmask 255.255.255.0
 
+sudo ifconfig eth0 down
+sudo ifconfig eth0 192.168.123.162/24
+sudo ifconfig eth0 up
